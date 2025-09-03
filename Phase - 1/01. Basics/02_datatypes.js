bdc5911f240a2a77033adee86319c26a51d0987a
partho <parthokumar702@gmail.com>
2025-09-03 11:44:13 +0600

    datatypes are done

---
+++ Phase - 1/01. Basics/02_datatypes.js
@@ -0,0 +1,22 @@
+let Name = "Partho";
+console.log("Name =", Name, "Type of Name =", typeof Name);
+
+let Age = 25;
+console.log("Age =", Age, "Type of Age =", typeof Age);
+
+let isStudent = true;
+console.log("isStudent =", isStudent, "Type of isStudent =", typeof isStudent);
+
+let address = null;
+console.log("address =", address, "Type of address =", typeof address); // typeof null is 'object' due to a historical bug in JavaScript
+
+let phoneNumber; // undefined
+console.log("phoneNumber =", phoneNumber, "Type of phoneNumber =", typeof phoneNumber);
+
+let bigNumber = 1234567890123456789012345678901234567890; // BigInt
+console.log("bigNumber =", bigNumber, "Type of bigNumber =", typeof bigNumber);
+
+/* we explore
+1. Different data types in JavaScript
+2. typeof operator
+*/
