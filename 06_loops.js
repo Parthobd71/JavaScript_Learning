76eeeeb1a1b5ada010e3d099438c08af096d6a65
partho <parthokumar702@gmail.com>
2025-09-03 21:52:26 +0600

    ok

---
+++ 06_loops.js
@@ -0,0 +1,12 @@
+// for loop
+for (let i = 0; i < 10; i++) {
+    console.log("Iteration number: " + i);
+}
+
+// for loop with break
+for(let i=0; i<10;i++){
+    if(i===5) break;
+    console.log("Intregation with break " , i +1);
+    
+}
+
