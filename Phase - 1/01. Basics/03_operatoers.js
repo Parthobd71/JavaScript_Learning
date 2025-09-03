8aa3eeb378ec9e908ef92e5549dc1c3030c1570a
partho <parthokumar702@gmail.com>
2025-09-03 11:58:32 +0600

    Opetatores done

---
+++ Phase - 1/01. Basics/03_operatoers.js
@@ -0,0 +1,56 @@
+let a = 5;
+b  = 10;
+
+console.log("a + b =", a + b); // Addition
+console.log("a - b =", a - b); // Subtraction
+console.log("a * b =", a * b); // Multiplication
+console.log("b / a =", b / a); // Division
+console.log("b % a =", b % a); // Modulus
+console.log("a ** 2 =", a ** 2); // Exponentiation
+
+// Comparison Operators
+console.log("a == b:", a == b); // Equal to
+console.log("a != b:", a != b); // Not equal to
+console.log("a === b:", a === b); // Strict equal to
+console.log("a !== b:", a !== b); // Strict not equal to
+console.log("a > b:", a > b); // Greater than
+console.log("a < b:", a < b); // Less than
+console.log("a >= b:", a >= b); // Greater than or equal to
+console.log("a <= b:", a <= b); // Less than or equal to
+
+// Logical Operators
+let isAdult = true;
+let hasID = false;
+console.log("isAdult && hasID:", isAdult && hasID); // Logical AND
+console.log("isAdult || hasID:", isAdult || hasID); // Logical OR
+console.log("!isAdult:", !isAdult); // Logical NOT
+
+// assignment Operators
+let c = 5;
+
+c += 3;
+console.log("c += 3:", c); // c += 3
+c -= 2;
+console.log("c -= 2:", c); // c -= 2
+c *= 4;
+console.log("c *= 4:", c); // c *= 4
+c /= 2;
+console.log("c /= 2:", c); // c /= 2
+// c %= 3;
+console.log("c %= 3:", c); // c %= 3
+c**= 3;
+console.log("c **= 3:", c); // c **= 3
+
+
+// Ternarty Operator
+let age = 10;
+let state = age >= 18 ? "You are an adult." : "You are a minor.";
+console.log(state);
+
+/* we explore
+1. Arithmetic Operators
+2. Comparison Operators
+3. Logical Operators
+4. Assignment Operators
+5. Ternary Operator
+*/
