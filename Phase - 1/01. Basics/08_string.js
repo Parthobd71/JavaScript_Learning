41a578a4403c8187ebd32b9b25fe061071d8c0ae
partho <parthokumar702@gmail.com>
2025-09-04 23:41:55 +0600

    ok

---
+++ Phase - 1/01. Basics/08_string.js
@@ -0,0 +1,19 @@
+let str1 = "Hello World";
+let str2 = 'hello js';
+let str3 = `hello template literal`;
+
+// console.log(str1, str2, str3);
+
+// console.log('Length of str1:', str1.length);
+// console.log('Character at index 0 of str1:', str1.charAt(0));
+// console.log('last character of str1:', str1[str1.length - 1]);
+
+
+// console.log('Index of "o" in str1:', str1.indexOf('o'));
+
+let name = "pk";
+let message = `hello ${name}, welcome to js`;
+console.log(message);
+
+
+
